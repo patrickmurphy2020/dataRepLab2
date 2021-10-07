bac44736c9a5cf795564d0c0df3f7f0567f7d21c
patrickmurphy2020 <g00379725@gmit.ie>
2021-10-07 11:02:52 +0100

    hopefully this works

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+import { Header } from './components/header';
+import { Footer } from './components/footer';
+import { Content } from './components/content';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav } from 'react-bootsrap';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+
+        <div className="App">
+
+          <Navbar bg="primary" variant="dark">
+            <Container>
+              <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+              <Nav className="me-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/read">Read</Nav.Link>
+                <Nav.Link href="/create">Create</Nav.Link>
+              </Nav>
+            </Container>
+          </Navbar>
+
+          <br />
+          <Switch>
+            <Route path='/' component={Content} exact />
+            <Route path='/create' component={Header} exact />
+            <Route path='/read' component={Footer} exact />
+          </Switch>
+
+
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+export class Footer extends React.Component{
+    render() {
+        return (
+            <div>
+                <h1>This is a footer component</h1>
+            </div>
+        );
+    }
+}
